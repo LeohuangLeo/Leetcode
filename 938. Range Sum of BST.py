4418b85da3b44801a9531812948dfa0574ed52b1
LeohuangLeo <47164895+LeohuangLeo@users.noreply.github.com>
2019-10-04 15:51:14 +0800

    Add files via upload

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rangeSumBST(self, root, L, R):
+        self.ans = 0
+        def CalSum(node):
+            if node:
+                if node.val >= L and node.val <= R:
+                    self.ans += node.val
