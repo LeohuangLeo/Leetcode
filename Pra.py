ffbd53d4560b5bc807edd0eb0041b59c4771ab41
LeohuangLeo <47164895+LeohuangLeo@users.noreply.github.com>
2019-10-04 17:38:03 +0800

    Add files via upload

---
+++ Pra.py
@@ -0,0 +1,9 @@
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        lis = []
+        if not root:
+            return lis
+        lis.append(root.val)
+        for children in root.children:
+            lis += self.preorder(children)
+        return lis
