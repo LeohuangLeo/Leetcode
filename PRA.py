5a104234e58d3f873cd17bac11f4975c5ba73656
LeohuangLeo <47164895+LeohuangLeo@users.noreply.github.com>
2019-10-05 08:20:31 +0800

    Add files via upload

---
+++ PRA.py
@@ -0,0 +1,9 @@
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        lis = []
+        if not root:
+            return lis
+        lis = [root.val] + lis
+        for i in root.children[::-1]:
+            lis = self.postorder(i) + lis
+        return lis
