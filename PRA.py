4fb12fc0b5081b15ff3a5d489361e4b687f31619
LeohuangLeo <47164895+LeohuangLeo@users.noreply.github.com>
2019-10-15 00:15:03 -0700

    Add files via upload

---
+++ PRA.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        num_even = list(filter(lambda x:x%2==0, A))
+        num_odd = list(filter(lambda x:x%2!=0, A))
+        num_all = []
+        for i in range(len(num_even)):
+            num_all.append(num_even[i])
+            num_all.append(num_odd[i])
+        return num_all   
