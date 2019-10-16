f3ed5178c5b8db6f49af83d85d188d70b8095e5c
LeohuangLeo <47164895+LeohuangLeo@users.noreply.github.com>
2019-10-15 21:53:28 -0700

    Add files via upload

---
+++ PRA.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        lis = [0,1]
+        counter = 2
+        while counter != N:
+            lis.append(lis[counter-1]+lis[counter-2])
+            counter += 1 
+        
+        return lis[N-1]+lis[N-2]
